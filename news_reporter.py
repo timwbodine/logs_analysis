243a14a89def6e7259b56cdbcf00822ee807c5f3
vagrant <vagrant@vagrant.vm>
2018-03-02 22:34:09 +0000

    Add comments to news_reporter.py and views.py

--- news_reporter.py
+++ news_reporter.py
@@ -1,11 +1,21 @@
+"""news_reporter.py generates several tables from the newsdata.sql data, organizing data by most popular authors, most popular articles, and high error days."""
 #!/usr/bin/python
 import psycopg2
 from tabulate import tabulate
 
 
+def connect(database):
+"""connect to the database.  alternatively, print an error message."""
+    try:
+        db = psycopg2.connect("dbname={}".format(database))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("There was an error connecting to the database.")
+
 def get_most_popular_articles():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+"""get a list of the top 3 most popular articles by all time views"""
+    db, c = connect("news")
     c.execute("select articles.title, count(*) from articles, log where subst"
               "ring(log.path from '/article/(.*)')=articles.slug group by"
               " articles.title order by count desc limit 3;")
@@ -13,8 +23,8 @@ def get_most_popular_articles():
 
 
 def get_most_popular_authors():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+"""get a list of the top 4 most popular authors by all time views of articles written by them"""
+    db, c = connect("news")
     c.execute("select authors.name, views_by_author_id.num from views_by_auth"
               "or_id, authors where views_by_author_id.author = authors.id"
               " limit 4;")
@@ -22,15 +32,15 @@ def get_most_popular_authors():
 
 
 def get_high_error_days():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+"""find and list any days on which the percentage of connections resulting in error exceeded 1%"""
+    db, c = connect("news")
     c.execute("select views_per_day.day, float4((errors_per_day.errors * 100."
               "0)/views_per_day.views) as perc from errors_per_day, views_per"
               "_day where views_per_day.day = errors_per_day.day and float4(("
               "errors_per_day.errors * 100.0)/views_per_day.views) >= 1.0;")
     return c.fetchall()
 
-
+"""use tabulate library to format tables written to output.txt"""
 with open('output.txt', 'w') as output:
     lines = get_most_popular_articles()
     output.write('Most Popular Articles\n\n')
