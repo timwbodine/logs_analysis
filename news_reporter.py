072ed90ff0092c930fd734869a1846754d5d1aa3
skrimbleshanks <timwbodine@gmail.com>
2018-03-03 15:07:32 -0500

    Fix issues with error percentage and print to terminal

--- news_reporter.py
+++ news_reporter.py
@@ -30,30 +30,36 @@ def get_most_popular_authors():
     written by them"""
     db, c = connect("news")
     c.execute("select authors.name, views_by_author_id.num from views_by_auth"
-              "or_id, authors where views_by_author_id.author = authors.id"
-              " limit 4;")
+              "or_id, authors where views_by_author_id.author = authors.id;")
     return c.fetchall()
 
 
 def get_high_error_days():
-    """find and list any days on which the percentage of connections resulting in
-    error exceeded 1%"""
+    """find and list any days on which the percentage of connections resulting
+    in error exceeded 1%"""
     db, c = connect("news")
     c.execute("select views_per_day.day, float4((errors_per_day.errors * 100."
-              "0)/views_per_day.views) as perc from errors_per_day, views_per"
-              "_day where views_per_day.day = errors_per_day.day and float4(("
-              "errors_per_day.errors * 100.0)/views_per_day.views) >= 1.0;")
+              "0)/(views_per_day.views + errors_per_day.errors)) as perc from"
+              " errors_per_day, views_per_day where views_per_day.day"
+              " = errors_per_day.day and float4((errors_per_day.errors * "
+              "100.0)/(views_per_day.views + errors_per_day.errors)) >= 1.0;")
     return c.fetchall()
 
 
-"""use tabulate library to format tables written to output.txt"""
-with open('output.txt', 'w') as output:
-    lines = get_most_popular_articles()
-    output.write('Most Popular Articles\n\n')
-    output.write(tabulate(lines, headers=['Articles', 'Views']))
-    output.write('\n\nMost Popular Authors\n\n')
-    lines = get_most_popular_authors()
-    output.write(tabulate(lines, headers=['Authors', 'Views']))
-    output.write('\n\nHigh Error Days\n\n')
-    lines = get_high_error_days()
-    output.write(tabulate(lines, headers=['Day', '% Errors']))
+def execute(output_file):
+    with open(output_file, 'w') as output:
+        def print_and_write(line):
+            """use tabulate library to format tables written to output.txt. Also
+            print them to terminal."""
+            output.write(line)
+            print line
+        lines = get_most_popular_articles()
+        print_and_write('Most Popular Articles\n\n')
+        print_and_write(tabulate(lines, headers=['Articles', 'Views']))
+        print_and_write('\n\nMost Popular Authors\n\n')
+        lines = get_most_popular_authors()
+        print_and_write(tabulate(lines, headers=['Authors', 'Views']))
+        print_and_write('\n\nHigh Error Days\n\n')
+        lines = get_high_error_days()
+        print_and_write(tabulate(lines, headers=['Day', '% Errors']))
+execute('output.txt')
